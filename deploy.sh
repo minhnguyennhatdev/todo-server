ace0b15cc47068266fc4002b7aca2affc3572e62
Minh Nguyen <nhatminhnguyen061200@gmail.com>
2024-04-22 08:44:21 +0700

    todo client

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,15 @@ git fetch --all && git reset --hard @{u}
 
 echo "Deploying..."
 
-SERVICE='todo-server'
-PORT=8000
 
-docker stop $SERVICE
-docker rm $SERVICE
-docker build --tag $SERVICE .
-docker run --name $SERVICE -d -p $PORT:$PORT $SERVICE
+pm2 restart ecosystem.config.js
+
+# SERVICE='todo-server'
+# PORT=8000
+
+# docker stop $SERVICE
+# docker rm $SERVICE
+# docker build --tag $SERVICE .
+# docker run --name $SERVICE -d -p $PORT:$PORT $SERVICE
 
 echo "Done."
